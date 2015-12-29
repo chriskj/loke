0c678db10601e2185b0947ae23593334a92e3ec5
Vegar Westerlund <vegarwe@gmail.com>
2015-12-29 23:55:16 +0100

    Refactor loke
    - Give loke a class (with the possibility to have local data)
    - Allow for better encapsulation

--- loke.py
+++ loke.py
@@ -7,57 +7,66 @@ from difflib import SequenceMatcher as SM
 from config import config
 
 
-def handle_message(sc, event):
-    # Ignore own messages by bot
-    if event['user'] == config['ownid']:
-        return
+class Loke(object):
+    def __init__(self):
+        self.sc = None
 
-    # Open file containing auto-responses
-    with open(config['auto_response'], mode='r') as infile:
-        responses = json.load(infile)
+    def handle_message(self, event):
+        # Ignore own messages by bot
+        if event['user'] == config['ownid']:
+            return
 
-    # Loop through the dictionary from CSV file
-    for response in responses:
-        # Split the written message into a list (split by space)
-        for word in event['text'].split():
-            word = word.lower()
-            if response['type'] == 'ratio':
-                #print word, response['key'], SM(None, word, response['key']).ratio()
-                match = SM(None, word, response['key']).ratio() > 0.85
-            elif response['type'] == 'match':
-                match = response['key'] == word
-            else:
-                match = False
-            if match:
-                sc.api_call("chat.postMessage", as_user="true:", channel=event['channel'],
-                        text=response['response'].encode('utf-8'))
-                break # Don't repeat same response for multiple hits
+        # Open file containing auto-responses
+        with open(config['auto_response'], mode='r') as infile:
+            responses = json.load(infile)
 
-def handle_presence_change(sc, event):
-    # See if a user in list travelers becomes available
-    if event['user'] in config['list_travelers'] and event['presence'] == "active":
-        sc.api_call("chat.postMessage", as_user="true:", channel=config['chan_general'], text='<@%s> is alive!! Skal han booke fly mon tro?!' % event['user'])
+        # Loop through the dictionary from CSV file
+        for response in responses:
+            # Split the written message into a list (split by space)
+            for word in event['text'].split():
+                word = word.lower()
+                if response['type'] == 'ratio':
+                    #print word, response['key'], SM(None, word, response['key']).ratio()
+                    match = SM(None, word, response['key']).ratio() > 0.85
+                elif response['type'] == 'match':
+                    match = response['key'] == word
+                else:
+                    match = False
+                if match:
+                    self.sc.api_call("chat.postMessage", as_user="true:", channel=event['channel'],
+                            text=response['response'].encode('utf-8'))
+                    break # Don't repeat same response for multiple hits
 
-def main():
-    sc = SlackClient(config['token'])
-    if not sc.rtm_connect():
-        print "Connection Failed, invalid token?"
-        return
+    def handle_presence_change(self, event):
+        # See if a user in list travelers becomes available
+        if event['user'] in config['list_travelers'] and event['presence'] == "active":
+            self.sc.api_call("chat.postMessage", as_user="true:", channel=config['chan_general'], text='<@%s> is alive!! Skal han booke fly mon tro?!' % event['user'])
 
-    while True:
-        new_events = sc.rtm_read()
-        for event in new_events:
-            print event
-            try:
-                if event['type'] == "message":
-                    handle_message(sc, event)
+    def _get_today(self):
+        now = time.time()
+        return now - (now % (60*60*24))
 
-                if event['type'] == "presence_change":
-                    handle_presence_change(sc, event)
-            except KeyError:
-                # TODO(vegawe): When does this happen? Should not be necessary
-                print("Key not found in dict")
-        time.sleep(1)
+    def init(self):
+        self.sc = SlackClient(config['token'])
+        if not self.sc.rtm_connect():
+            raise SystemExit(1, "Connection Failed, invalid token?")
+        return self
+
+    def loop(self):
+        while True:
+            new_events = self.sc.rtm_read()
+            for event in new_events:
+                print event
+                try:
+                    if event['type'] == "message":
+                        handle_message(event)
+
+                    if event['type'] == "presence_change":
+                        handle_presence_change(event)
+                except KeyError:
+                    # TODO(vegawe): When does this happen? Should not be necessary
+                    print("Key not found in dict")
+            time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    Loke().init().main()
--- tests/loke_test.py
+++ tests/loke_test.py
@@ -9,40 +9,42 @@ import mock
 import unittest
 
 from config import config
-import loke
+from loke import Loke
 
 class LokeTestCase(unittest.TestCase):
     def setUp(self):
         config['db'] = os.path.join(test_folder, 'db.sqlite')
         self.sc_mock = mock.Mock()
+        self.loke = Loke()
+        self.loke.sc = self.sc_mock
 
     def test_no_auto_response_no_fjasefisken(self):
-        loke.handle_message(self.sc_mock,
-                {'user': 'U0HCKMF7B', 'text': 'Fjase fisken fjaser om fjasefisken', 'channel': 'general'})
+        self.loke.handle_message( {'user': 'U0HCKMF7B', 'text': 'Fjase fisken fjaser om fjasefisken', 'channel': 'general'})
         assert not self.sc_mock.api_call.called, 'method should not have been called'
 
     def test_auto_response_to_snylteagurk(self):
-        loke.handle_message(self.sc_mock,
-                {'user': 'U0HCKMF7B', 'text': 'hello snylteagurk tror jeg', 'channel': 'general'})
+        self.loke.handle_message( {'user': 'U0HCKMF7B', 'text': 'hello snylteagurk tror jeg', 'channel': 'general'})
         self.sc_mock.api_call.assert_called_once_with("chat.postMessage", as_user="true:",
                 channel='general', text='Sylteagurk er gr\xc3\xb8nnsaken som tok drepen p\xc3\xa5 de norr\xc3\xb8ne gudene...')
 
     def test_auto_response_multiple_match(self):
-        loke.handle_message(self.sc_mock,
-                {'user': 'U0HCKMF7B', 'text': 'hello snylteagurk tror jeg sylteagurk', 'channel': 'general'})
+        self.loke.handle_message( {'user': 'U0HCKMF7B', 'text': 'hello snylteagurk tror jeg sylteagurk', 'channel': 'general'})
         self.sc_mock.api_call.assert_called_once_with("chat.postMessage", as_user="true:",
                 channel='general', text='Sylteagurk er gr\xc3\xb8nnsaken som tok drepen p\xc3\xa5 de norr\xc3\xb8ne gudene...')
 
     def test_auto_response_with_comma(self):
-        loke.handle_message(self.sc_mock,
-                {'user': 'U0HCKMF7B', 'text': 'Hva med sylteagurk, komma?', 'channel': 'general'})
+        self.loke.handle_message( {'user': 'U0HCKMF7B', 'text': 'Hva med sylteagurk, komma?', 'channel': 'general'})
         self.sc_mock.api_call.assert_called_once_with("chat.postMessage", as_user="true:",
                 channel='general', text='Sylteagurk er gr\xc3\xb8nnsaken som tok drepen p\xc3\xa5 de norr\xc3\xb8ne gudene...')
 
     def test_auto_response_no_ratio_match_for_tur(self):
-        loke.handle_message(self.sc_mock,
-                {'user': 'U0HCKMF7B', 'text': 'utur', 'channel': 'general'})
+        self.loke.handle_message( {'user': 'U0HCKMF7B', 'text': 'utur', 'channel': 'general'})
         assert not self.sc_mock.api_call.called, 'method should not have been called'
+
+    def test_handle_presence_change_valid(self):
+        self.loke.handle_presence_change( {'user': 'dummy', 'presence': 'active'})
+        self.sc_mock.api_call.assert_called_once_with("chat.postMessage", as_user="true:",
+                channel=config['chan_general'], text='<@dummy> is alive!! Skal han booke fly mon tro?!')
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
