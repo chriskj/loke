ab97b9af82f6d56c77abcb41517a4403840bcd61
chriskj <christian@gjallarhorn.net>
2017-07-18 09:49:36 +0200

    Moved loop into main

--- loke.py
+++ loke.py
@@ -1,50 +1,39 @@
 # coding=UTF-8
-import time
-import json
 from slackclient import SlackClient
-from difflib import SequenceMatcher as SM
-import forecastio
-import re
-import operator
-from datetime import datetime
-
-from config import config
-
 
 class Loke(object):
-    def __init__(self):
+    def __init__(self, config):
         self.sc = None
-        self.presence_rate_limit = {}
         self.config = config
 
     def handle_message(self, event):
         return
 
     def handle_presence_change(self, event):
-        user = event['user']
+        return
 
     def init(self):
-        self.sc = SlackClient(config['token'])
+        self.sc = SlackClient(self.config['token'])
         if not self.sc.rtm_connect():
             raise SystemExit(1, "Connection Failed, invalid token?")
         return self
 
-    def loop(self):
-        while True:
-            new_events = self.sc.rtm_read()
-            for event in new_events:
-                print(event)
-                try:
-                    if event['type'] == "message":
-                        self.handle_message(event)
+    #def loop(self):
+    #    while True:
+    #        new_events = self.sc.rtm_read()
+    #        for event in new_events:
+    #            print(event)
+    #            try:
+    #                if event['type'] == "message":
+    #                    self.handle_message(event)
 
-                    if event['type'] == "presence_change":
-                        self.handle_presence_change(event)
-                except KeyError:
-                    # TODO(vegawe): When does this happen? Should not be necessary
-                    print("Key not found in dict")
-                    print(event)
-            time.sleep(1)
+    #                if event['type'] == "presence_change":
+    #                    self.handle_presence_change(event)
+    #            except KeyError:
+    #                # TODO(vegawe): When does this happen? Should not be necessary
+    #                print("Key not found in dict")
+    #                print(event)
+    #        time.sleep(1)
 
-if __name__ == "__main__":
-    Loke().init().loop()
+#if __name__ == "__main__":
+#    Loke().init().loop()
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 # coding=UTF-8
+
+from config import config
+
 from loke import Loke
 from seen import SeenLoke
 
-Loke = Loke().init().loop()
-print(Loke)
+import time
 
+Loke = Loke(config).init()
+
+while True:
+    new_events = Loke.sc.rtm_read()
+    for event in new_events:
+        print(event)
+        try:
+            if event['type'] == "message":
+                Loke.handle_message(event)
+
+            if event['type'] == "presence_change":
+                Loke.handle_presence_change(event)
+        except KeyError:
+            # TODO(vegawe): When does this happen? Should not be necessary
+            print("Key not found in dict")
+            print(event)
+    time.sleep(1)
 
 #if __name__ == "__main__":
 #    Loke().init().loop()
