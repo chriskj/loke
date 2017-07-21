39ba501adb251e2b84d88d7996a5a7a9f573b844
chriskj <christian@gjallarhorn.net>
2017-07-21 10:14:02 +0200

    Added comments

--- modules/atb.py
+++ modules/atb.py
@@ -27,24 +27,27 @@ class AtBHandler(LokeEventHandler):
         atbmatch = re.match(r'\.atb (.*)', event['text'], re.I)
         if atbmatch:
             stopname = atbmatch.group(1).lower()
-            with open(self.loke.config['atbstops'], 'r') as fil:
-                stops = json.load(fil)
 
-            if stopname in stops:
-                trips = self.atb.GetMultipleStopStatus(stops[stopname])
-                for stop in trips:
-                    message = '* %s (id: %s):*\n' % (stopname.title(), stop[0]['StopId'])
+            # Load a full dict of all stops/ids for AtB
+            with open(self.loke.config['atbstops'], 'r') as fil:
+                stops = json.load(fil) 
+
+            # Check if provided stopname exist in dict and list all trips for the stop ids related to the name
+            if stopname in stops: # Check if stopname is in dict
+                trips = self.atb.GetMultipleStopStatus(stops[stopname]) # Fetch data from AtB
+                for stop in trips: # We want one response to Slack for each stop
+                    message = '* %s (id: %s):*\n' % (stopname.title(), stop[0]['StopId']) # Message title
                     message += '```'        
-                    for trip in stop[:8]:
+                    for trip in stop[:8]: # Only fetch 8 for each stop
                         ca = ''
-                        if trip['RealTime'] is False:
+                        if trip['RealTime'] is False: # Add Ca prefix if realtime data is not present
                             ca = 'Ca '
-                        if trip['ArrivalMinutes'] == 0:
+                        if trip['ArrivalMinutes'] == 0: # Use 'nå' instead of number of minutes
                             message += '%snå - %s %s\n' % (ca, trip['LineNumber'], trip['LineDestination'])
-                        elif trip['ArrivalMinutes'] < 10:
+                        elif trip['ArrivalMinutes'] < 10: # Show number of minutes if it's less than 10 minutes
                             message += '%s%s min - %s %s\n' % (ca, trip['ArrivalMinutes'], trip['LineNumber'], trip['LineDestination'])
-                        else:
-                            message += '%s%s - %s %s\n' % (ca, trip['ArrivalTime'], trip['LineNumber'], trip['LineDestination'])
+                        else: # Show time if it's more than 10 minutes
+                            message += '%s%s - %s %s\n' % (ca, trip['DepartureTime'].strftime('%H:%M'), trip['LineNumber'], trip['LineDestination'])
                     message += '```'        
                     self.loke.sc.api_call("chat.postMessage", as_user="true:", channel=event['channel'], text=message)
 
@@ -64,10 +67,14 @@ class AtB(object):
         self.client = zeep.Client(wsdl=wsdlurl)
 
     def GetMultipleStopStatus(self, stoplist):
+        # Do one request to AtB containing multiple stops
+
+        # stoplist argument is a list of ids to query
         requestfilter = []
         for stop in stoplist:
             requestfilter.append({'MonitoringRef': stop})
             
+        # Generate query based on list of ids
         response = self.client.service.GetMultipleStopMonitoring(
             ServiceRequestInfo={
                 'RequestorRef':'Loke', 
@@ -81,36 +88,40 @@ class AtB(object):
             RequestExtension={}
         )
 
-        stops = []
+        stops = [] # A list to contain one entry pr stopid
         for stop in response.Answer.StopMonitoringDelivery:
-            trips = []
+            trips = [] # A list to contain one entry pr trip. Each entry is a dict
             for item in stop.MonitoredStopVisit:
-                if item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime is None:
+                # use expectedtime as common term, regardless if we have real time data available or not
+                if item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime is None: 
                     expectedtime = item.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime.replace(tzinfo=None)
                     real = False
                 else:    
                     expectedtime = item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime
                     real = True 
                 
-                time = expectedtime.strftime("%H:%M")
-                timediff = int((expectedtime - datetime.now()).seconds/60)
+                timediff = int((expectedtime - datetime.now()).seconds/60) # Number of minutes until departure
 
-                trip = {}
+                trip = {} # Dict containing all data for trip
                 trip['ArrivalMinutes'] = timediff
                 trip['RealTime'] = real
-                trip['ArrivalTime'] = time
+                trip['DepartureTime'] = expectedtime
                 trip['LineNumber'] = item.MonitoredVehicleJourney.LineRef
                 trip['LineOrigin'] = item.MonitoredVehicleJourney.OriginName
                 trip['LineDestination'] = item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay
                 trip['StopId'] = item.MonitoringRef
 
                 trips.append(trip)
-
+            
+            # Only add entry to the stops list if we have trips
             if len(trips) > 0:
                 stops.append(trips)    
         return stops
 
     def GetStopStatus(self, stopid):
+        # Do one request to AtB containing one stop id
+        
+        # Generate query based on id
         response = self.client.service.GetStopMonitoring(
             ServiceRequestInfo={
                 'RequestorRef':'Loke', 
@@ -123,9 +134,10 @@ class AtB(object):
             },
             RequestExtension={}
         )
-        trips = []
+        trips = [] # A list to contain one entry pr trip. Each entry is a dict
 
         for item in response.Answer.StopMonitoringDelivery[0].MonitoredStopVisit:
+            # use expectedtime as common term, regardless if we have real time data available or not
             if item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime is None:
                 expectedtime = item.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime.replace(tzinfo=None)
                 real = False
@@ -133,16 +145,16 @@ class AtB(object):
                 expectedtime = item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime
                 real = True 
             
-            time = expectedtime.strftime("%H:%M")
-            timediff = int((expectedtime - datetime.now()).seconds/60)
+            timediff = int((expectedtime - datetime.now()).seconds/60) # Number of minutes until departure
 
-            trip = {}
+            trip = {} # Dict containing all data for trip
             trip['ArrivalMinutes'] = timediff
             trip['RealTime'] = real
-            trip['ArrivalTime'] = time
+            trip['DepartureTime'] = expectedtime
             trip['LineNumber'] = item.MonitoredVehicleJourney.LineRef
             trip['LineOrigin'] = item.MonitoredVehicleJourney.OriginName
             trip['LineDestination'] = item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay
+            trip['StopId'] = item.MonitoringRef
 
             trips.append(trip)
 
